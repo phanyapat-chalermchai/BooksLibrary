822953106569802313f8bc7d03c28df48f9cf182
Phanyapat Chalermchai <81134953+phanyapat@users.noreply.github.com>
2023-06-07 17:30:31 +0700

    change to spring boot

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
 import { Provider } from './context/books';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const el = document.getElementById('root');
+const root = ReactDOM.createRoot(el);
+
 root.render(
   <Provider>
     <App />
